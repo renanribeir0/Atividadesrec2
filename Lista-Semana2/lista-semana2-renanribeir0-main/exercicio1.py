ee86f9a445bb1ecaf9478bc90144ad93f91e94e8
renanribeir0 <110369271+renanribeir0@users.noreply.github.com>
2023-06-18 20:23:14 -0300

    alterada
    alterada

--- Lista-Semana2/lista-semana2-renanribeir0-main/exercicio1.py
+++ Lista-Semana2/lista-semana2-renanribeir0-main/exercicio1.py
@@ -5,6 +5,11 @@
 
 
 # Crie a função que será avaliada no exercício aqui
+def conta_palavras_unicas(frase):
+    palavras = frase.split()
+    palavras_unicas = set(palavras)
+    quantidade_palavras_unicas = len(palavras_unicas)
+    return quantidade_palavras_unicas
 
 
 
